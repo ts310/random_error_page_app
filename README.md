6b48a2f17ef9ed3fc15b632caf54d2cfc3ce4c13
Tsuyoshi Saito <tsuyoshi.saito@enigmo.co.jp>
2015-10-20 21:00:09 +0900

    Added random error pages

---
+++ README.md
@@ -0,0 +1,16 @@
+## Random error page
+
+* can be used to test out how your app behaves under irregular response.
+
+## This is how you run this app
+
+```
+$ bundle install
+$ sudo bundle exec shotgun --port 80
+```
+
+```
+$ sudo vi /etc/hosts
+
+127.0.0.1  www.example.com
+```
--- app.rb
+++ app.rb
@@ -7,10 +7,41 @@ module Simple
   # Default application
   class App < Sinatra::Base
     get '*' do
+      method = [:random_refresh,
+                :error_404,
+                :maintentance].sample(1).first
+      send(method)
+    end
+
+    private
+
+    def random_refresh
+      if rand(1..10).even?
+        refresh
+      else
+        slim :index
+      end
+    end
+
+    def refresh
+      slim :refresh
+      headers \
+        'Cache-Control' => 'no-cache',
+        'Connection' => 'Close',
+        'Content-Type' => 'text/html',
+        'charset' => 'iso-8859-1',
+        'Pragma' => 'no-cache',
+        'Refresh' => '0.1'
+    end
+
+    def error_404
+      status 404
+      slim :error_404
+    end
+
+    def maintentance
       status 503
-      body 'Service temporary not available'
-      #status 500
-      #body 'Internal server error occured'
+      slim :error_503
     end
   end
 end
